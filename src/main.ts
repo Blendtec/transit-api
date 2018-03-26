ceba3edb794301d6ebd853d6e029aa974d18b626
Reid Kuttler <rkuttler@blendtec.com>
2018-03-26 08:59:37 -0600

    moved cors location to env file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.use(function(req, res, next) {
-		if (req.headers.origin === 'http://blendtec.test' || req.headers.origin === 'https://old.blendtec.com') {
+		if (req.headers.origin === process.env.API_LOCATION && req.method === 'POST') {
 		  res.header("Access-Control-Allow-Origin", req.headers.origin);
 		  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		} else if (req.method === 'POST') {
