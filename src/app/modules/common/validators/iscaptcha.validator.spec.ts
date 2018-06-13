096b102f64f7ca7fed59dd4cb57f61bcf81d5436
Stone C. Lasley <stonelasley@gmail.com>
2018-06-13 12:05:44 -0600

    chore(iscaptcha.validator.spec): remove unneeded parameters

--- src/app/modules/common/validators/iscaptcha.validator.spec.ts
+++ src/app/modules/common/validators/iscaptcha.validator.spec.ts
@@ -10,18 +10,18 @@ describe('IsCaptcha', () => {
 
   describe('defaultMessage', () => {
     it('should say failed', () => {
-      expect(isCaptcha.defaultMessage({})).toContain('failed');
+      expect(isCaptcha.defaultMessage()).toContain('failed');
     });
   });
 
   describe('validate', () => {
     it('test string should validate captcha', async () => {
-      const out = await isCaptcha.validate('asdf', {});
+      const out = await isCaptcha.validate('asdf');
       expect(out).toBe(true);
     });
 
     it('empty string should not validate captcha', async () => {
-      const out = await isCaptcha.validate('', {});
+      const out = await isCaptcha.validate('');
       expect(out).toBe(false);
     });
 
