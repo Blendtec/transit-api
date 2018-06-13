43d8ee23e607b44e1729b5d33899e313fcdf100f
Stone C. Lasley <stonelasley@gmail.com>
2018-06-13 12:03:34 -0600

    revert(main.ts): re-add request size limit specification

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 import { ValidationPipe } from './app/modules/common/pipes';
 import { useContainer } from 'class-validator';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
     useContainer(app.select(ApplicationModule), { fallback: true });
     app.useGlobalPipes(new ValidationPipe());
+    app.use(bodyParser.json({limit: '10mb'}));
+    app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
     app.use((req, res, next) => {
-        if (req.headers.origin === process.env.API_LOCATION) {
+        if (req.headers.origin === process.env.CLIENT_ORIGIN) {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         }
